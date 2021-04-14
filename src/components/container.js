8ca64409306d3196dbd025822ea825ec90aebfa8
Jeff Johnson <jojohnso101@gmail.com>
2021-04-14 17:58:50 +0200

    give name to exported component

--- src/components/container.js
+++ src/components/container.js
@@ -1,13 +1,15 @@
 import React from "react"
 import styled from "styled-components"
 
-const Container = styled.section`
+const StyledContainer = styled.section`
   max-width: 1040px; // large
   margin: 0 auto;
   padding: 0 20px;
   width: 100%;
 `
 
-export default ({ className, children }) => (
-  <Container className={className}>{children}</Container>
+const Container = ({ className, children }) => (
+  <StyledContainer className={className}>{children}</StyledContainer>
 )
+
+export default Container
